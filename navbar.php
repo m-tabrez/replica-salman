fcbfa38925b08d16feccf5a13d87551eae0a875d
m-tabrez <96100500+m-tabrez@users.noreply.github.com>
2023-10-19 22:09:02 +0530

    SiteForRef

---
+++ navbar.php
@@ -0,0 +1,27 @@
+<nav class="navbar navbar-default" role="navigation">
+
+<respond-cart-toggle></respond-cart-toggle>
+<respond-languages-toggle></respond-languages-toggle>
+
+<div class="navbar-header">
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+        <i class="fa fa-bars"></i>
+    </button>
+
+    <a class="navbar-brand" href="index"><img src="files/logo.jpg"></a>
+
+</div>
+<!-- /.navbar-header -->
+
+<div class="collapse navbar-collapse navbar-ex1-collapse">
+    <ul class="nav navbar-nav navbar-right">
+        <li class="active "><a href="index">Home</a></li>
+        <li><a href="about-us">About Us</a></li>
+        <li><a href="product-range">Products</a></li>
+        <li><a href="careers">Careers</a></li>
+        <li><a href="contact">Contact Us</a></li>
+    </ul>
+</div>
+<!-- /.navbar-collapse -->
+
+</nav>
